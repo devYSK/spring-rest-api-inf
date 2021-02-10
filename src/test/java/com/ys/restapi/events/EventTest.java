08f428afebee5f04624f3c70917f7c6f057f8f12
Young Soo Kim <41531594+devYSK@users.noreply.github.com>
2021-02-10 23:54:55 +0900

    EventTest
    builder test case

--- src/test/java/com/ys/restapi/events/EventTest.java
+++ src/test/java/com/ys/restapi/events/EventTest.java
@@ -1,6 +1,16 @@
 package com.ys.restapi.events;
 
 
-class EventTest {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder().build();
+
+        assertThat(event).isNotNull();
+    }
 }
