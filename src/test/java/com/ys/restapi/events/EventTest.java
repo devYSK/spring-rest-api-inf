6bbdf0e4cf3494b45d864d765b054066743c0c4f
Young Soo Kim <41531594+devYSK@users.noreply.github.com>
2021-02-11 23:34:22 +0900

    EventTest
    javabean test case

--- src/test/java/com/ys/restapi/events/EventTest.java
+++ src/test/java/com/ys/restapi/events/EventTest.java
@@ -9,8 +9,21 @@ public class EventTest {
 
     @Test
     public void builder() {
-        Event event = Event.builder().build();
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API developer")
+                .build();
 
         assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        String name = "Event";
+        Event event = new Event();
+        event.setName(name);
+
+
+        assertThat(event.getName()).isEqualTo(name);
     }
 }
