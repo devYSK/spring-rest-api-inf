16ded0965ddc9e04fa5e8c87cd2828b6de4c9ca5
Young Soo Kim <41531594+devYSK@users.noreply.github.com>
2021-02-12 19:32:05 +0900

    EventControllerTests.java 테스트 메서드 생성
    createEvent() 메서드 테스트

--- src/test/java/com/ys/restapi/events/EventControllerTests.java
+++ src/test/java/com/ys/restapi/events/EventControllerTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +24,7 @@ public class EventControllerTests {
     public void createEvent() throws Exception {
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
+                .accept(MediaTypes.HAL_JSON))
                 .andExpect(status().isCreated());  // 201 응답
     }
 }
